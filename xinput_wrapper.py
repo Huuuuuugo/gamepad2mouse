701d6af63645e71f7293d9622d04f6f8a20cb3d8
Hugo Messias <hugomessias9@gmail.com>
2024-05-31 17:24:51 -0300

    created the Notify() class: this class allows the program to send a notification to the gamepad in the form of vibrations with predetermined intensity and timing
    it also makes use of threads to run the notification without interrupting the flow of the program
    
    for now, there are only two types of notification, one for ON, and other for OFF, both are represented by boolean values and intended to be used when activating or deactivating the script on main.py

--- xinput_wrapper.py
+++ xinput_wrapper.py
@@ -1,16 +1,85 @@
-# source: https://stackoverflow.com/questions/43414189/how-can-i-use-xinput-in-python-without-pygame-to-sniff-button-presses-on-the
-# source: https://pastebin.com/8KDYbpaj
-
+# adapted from: https://pastebin.com/8KDYbpaj
+# TODO: change 'GamepadControls.list_gamepads()' behaviour from "create list of gamepads" to "update list of gamepads"
+#   this will allow it to be called inside of a loop without creating new gamepad and notify objects on every single cycle
+#   which, in turn, will allow the thread logic of the notify object to work propperly on this cenario
 
 from __future__ import absolute_import, division
-from itertools import count
-from operator import itemgetter
-import ctypes
- 
 from xinput import XInputJoystick, get_bit_values, XINPUT_GAMEPAD
+import ctypes
+import threading
+import time
+
+# uses the vibration of the controller to notify if something was activated or deactivated
+# this class must be instantiated and it's object must be used as a thread
+class Notify():
+    def __init__(self, gamepad=0):
+        self.gamepad = gamepad
+        # stores the state of the last succesful call to Notify()
+        # it's used to allow notifications of the other state to run above the current one, without waiting for it to finish
+        self.last_state = None
+        # flag that stores the current state of the thread (running or not running)
+        # used in combinaton of 'lock' to avoid cuncurrent calls to the same state of notification
+        self.is_running = False
+        # lock used when changing the state of the 'is_running' flag
+        # used in combinaton of 'is_running' to avoid cuncurrent calls to the same state of notification
+        self.lock = threading.Lock()
+        # stores the number of the current trhead call
+        # used to avoid that a previous thread call changes the state of the 'is_runnig' flag
+        self.last_call = 0
+       
+    def run(self, state: bool):
+        # while using the lock:
+        with self.lock:
+            # check if the current state is different from the last one
+            # if it's different, this is a Notify() call of other state, and therefore should run without waiting for the last thread call to finish
+            # if it's not different, check if the 'is_running' flag before proceding
+            if self.last_state is state:
+                if self.is_running:
+                    # print("already running!")
+                    return
+                self.is_running = True
+
+        # update state
+        self.last_state = state
+        # assign 'current_call' and update 'last_call'
+        self.last_call = current_call = self.last_call + 1
+        if state:
+            # interrupt current thread call if another call was made (ie if it's not the latest call anymore)
+            if current_call is not self.last_call:
+                # print("state changed!")
+                return
+            self.gamepad.set_vibration(1,1)
+            time.sleep(0.4)
+        else:
+            # interrupt current thread call if another call was made (ie if it's not the latest call anymore)
+            if current_call is not self.last_call:
+                # print("state changed!")
+                return
+            self.gamepad.set_vibration(0.1,1)
+            time.sleep(0.9)
+        # interrupt current thread call if another call was made (ie if it's not the latest call anymore)
+        if current_call is not self.last_call:
+            # print("state changed!")
+            return
+        self.gamepad.set_vibration(0,0)
+        time.sleep(1.5)
+
+        # while using the lock
+        with self.lock:
+            # check if there was still no other calls after this one before trying to change the 'is_running' flag
+            if current_call is self.last_call:
+                # change the 'is_runing' flag state, to allow the thread to run for same state again
+                self.is_running = False
+                # print("end of thread!", current_call)
+            # else:
+            #     print("changed end of thread!", current_call)    
+
+    def __call__(self, state: bool):
+        T_notify = threading.Thread(target=self.run, args=(state,), daemon=True)
+        T_notify.start()
  
- 
-class GamepadControls(XInputJoystick):
+
+class GamepadControls(XInputJoystick, Notify):
     """Wrapper for the XInputJoystick class to avoid using events."""
     
     def __init__(self, *args, **kwargs):
@@ -19,6 +88,7 @@ class GamepadControls(XInputJoystick):
         except (IndexError, AttributeError):
             raise ValueError('use the result from GamepadControls.list_gamepads() to initialize the class')
         super(GamepadControls, self).__init__(device_number, *args[1:], **kwargs)
+        self.notify = Notify(args[0])
  
     @classmethod
     def list_gamepads(self):
@@ -69,11 +139,15 @@ class GamepadControls(XInputJoystick):
 if __name__ == '__main__':
     import time
 
-    #Example usage
     gamepads = GamepadControls.list_gamepads()
     while True:
-        with gamepads[0] as gamepad_input:
-            buttons = gamepad_input.get_button()
-            axis = gamepad_input.get_axis()
-            print(f"{buttons}\n{axis}{' '*(132-len(str(axis))) + '\x1b[A\r'}", end='', flush=True)
-        time.sleep(1/60)
+        for gamepad in gamepads:
+            with gamepad as gamepad_input:
+                buttons = gamepad_input.get_button()
+                axis = gamepad_input.get_axis()
+                if buttons['select'] and buttons['A']:
+                    gamepad_input.notify(True)
+                if buttons['select'] and buttons['B']:
+                    gamepad_input.notify(False)
+                print(f"{buttons}\n{axis}{' '*(132-len(str(axis))) + '\x1b[A\x1b[A\r'}", end='', flush=True)
+            time.sleep(1/60)
